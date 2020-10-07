92bc9727b76ed6b0550dff832025bc7c57ecf0ea
Julio Campuzano <julio.campuzano@expertscoding.es>
2020-10-07 09:06:28 +0200

    added some fixes

--- src/DataMasker.Function/DataMask.cs
+++ src/DataMasker.Function/DataMask.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Web.Http;
 using DataMasker.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -29,23 +30,34 @@ namespace DataMasker.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
             HttpRequest req, ILogger logger, ExecutionContext context)
         {
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                logger.LogInformation("Starting execution...");
 
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(context.FunctionAppDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-            
-            var config = await ReadConfig(configuration);
+                logger.LogDebug("Getting configuration");
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(context.FunctionAppDirectory)
+                    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
 
-            await _runner.Execute(config);
-            
-            return new OkResult();
-            
+                logger.LogDebug("Getting masking configuration");
+                var config = await ReadConfig(configuration, logger);
+
+                await _runner.Execute(config);
+
+                logger.LogInformation("Execution complete.");
+
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "error");
+                return new InternalServerErrorResult();
+            }
         }
 
-        private static async Task<Config> ReadConfig(IConfiguration configuration)
+        private async Task<Config> ReadConfig(IConfiguration configuration, ILogger logger)
         {
             var connectionString = configuration.GetConnectionString("StorageAccount");
             var containerName = configuration.GetValue<string>("containerName");
@@ -54,11 +66,20 @@ namespace DataMasker.Function
             var storageAccount = CloudStorageAccount.Parse(connectionString);
 
             // Create the destination blob client
+            logger.LogDebug("Creating blob client...");
             var blobClient = storageAccount.CreateCloudBlobClient();
+            logger.LogDebug("Creating container reference...");
             var container = blobClient.GetContainerReference(containerName);
+            logger.LogDebug("Creating blob reference...");
             var configBlob = container.GetBlobReference(blobName);
-
+            if (!await configBlob.ExistsAsync())
+            {
+                logger.LogError("Config file can not be found!");
+                throw new FileNotFoundException();
+            }
+            logger.LogDebug("Reading file...");
             await using var stream = await configBlob.OpenReadAsync();
+            logger.LogDebug("Loading config...");
             var config = Config.Load(stream);
             return config;
         }
